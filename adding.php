38245ac60b171b920658367eaa4ae968e4f96c22
binnllii <ybl@uoregon.edu>
2018-02-19 11:04:33 -0800

    Everything in file

---
+++ adding.php
@@ -0,0 +1,63 @@
+<?php
+
+
+
+include('connectiondata.txt');
+// include('')
+
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die('Error connecting to MySQL server.');
+
+// $conn = mysqli_connect('ix.cs.uoregon.edu','ybl','413290')
+
+// if(!$conn){
+// 	echo 'not connected to server';
+// }
+// if(!mysql_select_db($conn, housemap)){
+// 	echo 'not selected';
+// }
+
+
+
+$Address = $_POST['address'];
+$Apt = $_POST['apt'];
+$City = $_POST['city'];
+$State = $_POST['state'];
+$Zip = $_POST['zip'];
+$bed = $_POST['bed'];
+$bath = $_POST['bath'];
+$sqft = $_POST['sqft'];
+$hometype = $_POST['hometype'];
+$price = $_POST['price'];
+
+
+
+$address = mysqli_real_escape_string($conn, $address);
+
+$sql =  "INSERT INTO house (address, apt_number, city, state, zip, price, bed, bath, square_feet, home_type) 
+      VALUES ('$Address','$Apt','$City','$State', '$Zip','$price','$bed','$bath','$sqft','$hometype')";
+
+$result = mysqli_query($conn, $sql)
+or die(mysqli_error($conn));
+
+
+
+
+header("refresh:2; url:index.html");
+
+mysqli_close($conn);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="refresh" content="2;url = index.html">
+</head>
+</html>
+
+
+
+
---
+++ housemap.php
@@ -0,0 +1,38 @@
+<?php
+
+include('connectionData.txt');
+
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die('Error connecting to MySQL server.');
+
+
+  
+$address = $_POST['address'];
+
+$address = mysqli_real_escape_string($conn, $address);
+// this is a small attempt to avoid SQL injection
+// better to use prepared statements
+
+$query = "SELECT h.address, h.apt_number, h.city, h.state, h.zip
+			FROM housemap.house h 
+			WHERE address like  ";
+$query = $query. "'%$address%'";
+
+$result = mysqli_query($conn, $query)
+or die(mysqli_error($conn));
+
+
+print "<pre>";
+while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+  {
+    print "\n";
+    print "$row[address] $row[apt_number] $row[city] $row[state] $row[zip]";
+  }
+print "</pre>";
+
+mysqli_free_result($result);
+
+mysqli_close($conn);
+
+?>
+
